2164d2ceef6df8354a12b728e553435b199c46b8
Nurnasib <binsharf15@gmail.com>
2022-02-16 13:19:42 +0600

    lllllll

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+@section('title')
+    About
+@endsection
+@section('body')
+    <h1>About page</h1>
+@endsection
---
+++ resources/views/bitm.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+
+@section('title')
+    BITM
+@endsection
+@section('body')
+    <h1>BITM page</h1>
+@endsection
